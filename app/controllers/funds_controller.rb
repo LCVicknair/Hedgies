7fd6ac734370e0504027b5fe632a2ed376b37256
Lauren Vicknair <lvicknair@greenhouse.io>
2016-03-07 17:33:05 -0500

    added postgres

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -5,8 +5,15 @@ def index
 	#shows all of the available funds
 end
 
+def show
+	@fund = Fund.find(params[:id])
+end
 
 
+
+
+
+private
 
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
   def index
   end
 
+  def show
+
+  end
+
+
 
 
 
