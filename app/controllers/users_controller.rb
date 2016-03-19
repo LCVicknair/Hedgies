934c1ed3f2736206f5a33081225592c6a033f752
Lauren Vicknair <lvicknair@greenhouse.io>
2016-03-19 16:22:23 -0400

    modified navbard for devise logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
 
   def index
+  	@user = User.find(params[:user_id])
   end
 
   def show
-
+  	@user = User.find(params[:id])
+    @funds = @user.funds
   end
 
 
